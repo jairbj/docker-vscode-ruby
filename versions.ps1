50c9965193f3d5d84690e41cdda2415796f691ae
Jair Junior <jairbj@gmail.com>
2020-06-06 18:15:05 -0300

    Add Ruby Version 2.7.1.

--- versions.ps1
+++ versions.ps1
@@ -2,7 +2,11 @@ $image = "jairbj/vscode-ruby"
 $versions = @(
     @{
         tag="latest"
-        ruby_version="2.6.3"
+        ruby_version="2.7.1"
+    },
+    @{
+        tag="2.7.1"
+        ruby_version="2.7.1"
     },
     @{
         tag="2.6.3"
